858ae480afdad14ebb68a045c54b269179daa19d
lightwick <smallyk99@gmail.com>
2024-07-14 23:10:21 +0900

    Added Emulator and IP of running server for testing purposes

---
+++ arduino/arduinoEmulator.py
@@ -0,0 +1,65 @@
+import socket
+import time
+import re
+import os
+
+# Function to read server and port from config.h
+def read_config(config_file_path):
+    server = ''
+    server_port = 0
+
+    server_re = re.compile(r'const char\* server = "(.*)";')
+    server_port_re = re.compile(r'const int serverPort = (\d+);')
+
+    with open(config_file_path, 'r') as file:
+        for line in file:
+            server_match = server_re.search(line)
+            server_port_match = server_port_re.search(line)
+
+            if server_match:
+                server = server_match.group(1)
+
+            if server_port_match:
+                server_port = int(server_port_match.group(1))
+    # Output the parsed values
+    print(f'Server: {server}')
+    print(f'Server Port: {server_port}')
+    return server, server_port
+
+# Path to the config.h file
+config_file_path = './config.h'
+server, server_port = read_config(config_file_path)
+
+# Initialize TCP client
+def connect_to_server():
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    try:
+        client.connect((server, server_port))
+        print("Connected to server")
+        return client
+    except Exception as e:
+        print(f"Failed to connect to server: {e}")
+        return None
+
+def send_data(client, message):
+    try:
+        client.sendall(message.encode('utf-8'))
+        print(f"Sent: {message}")
+    except Exception as e:
+        print(f"Failed to send data: {e}")
+        print("Attempting to connect to server...")
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        time.sleep(1) # avoid overflooding the server
+        send_data(client, message)
+
+def emulate_arduino():
+    client = connect_to_server()
+    if client is None:
+        print("Failed to connect to server...Quitting Program...")
+        return
+    while True:
+        message = input("Enter the sensor data to send: ")
+        send_data(client, message)
+
+if __name__ == "__main__":
+    emulate_arduino()
--- arduino/config_example.h
+++ arduino/config_example.h
@@ -3,7 +3,7 @@
 
 const char* ssid = "YOUR_SSID_HERE";
 const char* pass = "YOUR_PASSWORD_HERE";
-const char* server = "192.168.0.104";
+const char* server = "175.205.85.190";
 const int serverPort = 8000;
 
 #endif
